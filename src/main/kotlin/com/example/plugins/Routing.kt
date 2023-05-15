7a85257508b68559bbc6e166adb57da9207e7631
hyunxhri <sbulpe@iesrafaelalberti.es>
2023-05-15 12:24:49 +0200

    intento de seguir, tiene errores

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,18 +1,39 @@
 package com.example.plugins
 
+import com.example.models.*
 import io.ktor.server.application.*
+import io.ktor.server.freemarker.*
 import io.ktor.server.http.content.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.server.util.*
 
 fun Application.configureRouting() {
     routing {
+        // ...
         get("/") {
-            call.respondText("Hello World!")
+            call.respondRedirect("articles")
         }
-        // Static plugin. Try to access `/static/index.html`
-        static("/static") {
-            resources("static")
+        route("articles") {
+            get {
+                call.respond(FreeMarkerContent("index.ftl", mapOf("articles" to articles)))
+            }
+            get("new") {
+                // Show a page with fields for creating a new article
+            }
+            post {
+                // Save an article
+            }
+            get("{id}") {
+                // Show an article with a specific id
+            }
+            get("{id}/edit") {
+                // Show a page with fields for editing an article
+            }
+            post("{id}") {
+                // Update or delete an article
+            }
         }
     }
 }
